2ec2bd7c8e7433078eb5c4efd62d02c8691f48e8
Caua Muller Henkel <cauamhenkel@gmail.com>
2026-07-22 01:32:49 -0300

    Pequena adição de direção para não me esquecer depois XD

--- include/entity.hpp
+++ include/entity.hpp
@@ -4,10 +4,16 @@
 #include "my_game.hpp"
 #include "map.hpp"
 
+enum class Direction{
+    Left,
+    Right
+};
+
 class Entity{
 private:
     int m_posX {};
     int m_posY {};
+    Direction direction {Direction::Right};
 
 public:
 
